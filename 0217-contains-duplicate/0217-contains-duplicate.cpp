1c42314ae8de219b05c9dbf37a320906454ec6be
Shamathmika <vshamathmika@gmail.com>
2025-09-27 18:24:00 -0700

    Time: 52 ms (48.04%), Space: 90.7 MB (60.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,17 +4,13 @@ public:
         unordered_map<int, int> map;
         for(int i = 0; i < nums.size(); i++) {
             if(map.find(nums[i]) != map.end()) {
-                map[nums[i]] += 1;
+                return true;
             }
             else {
                 map[nums[i]] = 1;
             }
         }
 
-        if(map.size() == nums.size()) {
-            return false;
-        }
-
-        return true;
+        return false;
     }
 };
