9323539eb73339d5339477333c1083481990c972
Shamathmika <vshamathmika@gmail.com>
2026-07-11 13:57:52 -0700

    Time: 0 ms (100%), Space: 20.7 MB (16.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,18 +1,31 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         """
-        We can solve this using a hashmap. Have two variables for tracking. One to just traverse
+        We can solve this using a hashset. Have two variables for tracking. One to just traverse
         through the array, the other to maintain the position of the next element to replace (k).
-        We return k in the end.
+        We return k in the end. This is the brute force approach where we need extra space.
+        We can avoid the hash set completely since the array is in non-decreasing order. So we
+        only need to check with the previous element. If not, replace with k else just keep
+        traversing
         """
-        k = 0
-        m = set()
-        for i in range(len(nums)):
-            if nums[i] in m:
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
                 continue
-            m.add(nums[i])
             nums[k] = nums[i]
             k += 1
-        
         return k
-        # TC: O(N), SC: O(N-k)
+        # TC: O(N), SC:O(1)
+
+        # # Brute force
+        # k = 0
+        # m = set()
+        # for i in range(len(nums)):
+        #     if nums[i] in m:
+        #         continue
+        #     m.add(nums[i])
+        #     nums[k] = nums[i]
+        #     k += 1
+        
+        # return k
+        # # TC: O(N), SC: O(N-k)
