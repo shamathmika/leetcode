fe03e9c77abb44a986dbfbad44774336041c42d9
Shamathmika <vshamathmika@gmail.com>
2026-08-25 23:24:42 -0700

    Time: 4 ms (44.78%), Space: 19.2 MB (78.59%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,29 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
+        This is very easy. Two ways - copy the number to an array or string and reverse it to get
+        the actual number, then add it really and create a new linked list with each of the digits.
+        This adds O(N) space. So instead we can use two pointers and just create the new ll directly
+        by just maintaining a sum and carry variables
         """
+        ret = ListNode()
+        cur = ret
         carry = 0
-        result = new_node = ListNode(0) # dummy node to start with
-
-        while l1 or l2 or carry: # goes on until both of the numbers are over or there is a last carry
-            if l1:
-                carry += l1.val  # carry = 2 | 4 | 4
-                l1 = l1.next
-            if l2:
-                carry += l2.val  # carry = 7 | 6 | 8
-                l2 = l2.next
-            
-            carry, val = divmod(carry, 10) # carry = 7/10 = 0, val = 7%10 = 7 | carry = 10/10 = 1, val = 10%10 = 0 | carry = 0, val = 8
-
-            new_node.next = ListNode(val) # creates a new node with the value and ensures the current one points to it
-            new_node = new_node.next
-
-        return result.next # ignore the dummy node with 0 created initially
+        while l1 != None or l2 != None or carry != 0: # This is same as while l1 and l2 or carry > 0
+            s = 0
+            s += l1.val if l1 else 0
+            s += l2.val if l2 else 0
+            s += carry
+            carry = s // 10
+            cur.next = ListNode(s % 10)
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return ret.next
+        # TC: O(N), SC: O(1)
